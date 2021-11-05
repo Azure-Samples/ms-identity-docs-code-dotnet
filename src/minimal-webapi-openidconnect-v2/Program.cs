82ae1648a598ce15508ffd53dd7998881c0eac45
Fernando Antivero <v-feanti@microsoft.com>
2021-11-05 18:58:41 +0000

    Address PR FeedbackL: remove swagger

--- src/minimal-webapi-openidconnect-v2/Program.cs
+++ src/minimal-webapi-openidconnect-v2/Program.cs
@@ -10,15 +10,8 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 builder.Services.AddAuthorization();
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
 app.UseHttpsRedirection();
 
