2591b1b7795255e1c46d7decc0fc0dda2c4169d9
Fernando Antivero <v-feanti@microsoft.com>
2021-11-05 18:03:34 +0000

    annotate code

--- src/protect-api/Program.cs
+++ src/protect-api/Program.cs
@@ -1,26 +1,39 @@
+// <ms_docref_import_types>
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.Identity.Web;
+// </ms_docref_import_types>
 
 var builder = WebApplication.CreateBuilder(args);
 
+// <ms_docref_add_msal>
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
+// </ms_docref_add_msal>
+// <ms_docref_add_default_authz_policies>
 builder.Services.AddAuthorization();
+// </ms_docref_add_default_authz_policies>
 
 var app = builder.Build();
 
 app.UseHttpsRedirection();
 
+
+// <ms_docref_enable_authn_capabilities>
 app.UseAuthentication();
+// </ms_docref_enable_authn_capabilities>
+// <ms_docref_enable_authz_capabilities>
 app.UseAuthorization();
+// </ms_docref_enable_authz_capabilities>
 
 var summaries = new[]
 {
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
+
+// <ms_docref_protect_endpoint>
 app.MapGet("/weatherforecast", [Authorize] () =>
 {
     var forecast =  Enumerable.Range(1, 5).Select(index =>
@@ -34,6 +47,7 @@ app.MapGet("/weatherforecast", [Authorize] () =>
     return forecast;
 })
 .WithName("GetWeatherForecast");
+// </ms_docref_protect_endpoint>
 
 app.Run();
 
