b80e541741da48d91bcc0de66482de63a81744d6
Fernando Antivero <v-feanti@microsoft.com>
2021-12-01 13:57:43 -0300

    add user sign in exp

--- src/sign-in-xplat/MainPage.xaml.cs
+++ src/sign-in-xplat/MainPage.xaml.cs
@@ -1,24 +1,131 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.Graph;
+using Microsoft.Identity.Client;
 using Microsoft.Maui.Controls;
 using Microsoft.Maui.Essentials;
 
 namespace Mobile
 {
-	public partial class MainPage : ContentPage
-	{
-		int count = 0;
+    public partial class MainPage : ContentPage
+    {
+        //Set the scope for API call to user.read
+        private string[] scopes = new string[] { "user.read" };
+        private const string ClientId = "[Enter the Client Id (Application ID obtained from the Azure portal), e.g. ba74781c2-53c2-442a-97c2-3d60re42f403]";
+        private const string Tenant = "[Copy the Tenant Id from the Azure portal]";
+        private const string Authority = "https://login.microsoftonline.com/" + Tenant;
 
-		public MainPage()
-		{
-			InitializeComponent();
-		}
+        // The MSAL Public client app
+        private static IPublicClientApplication PublicClientApp;
 
-		private void OnCounterClicked(object sender, EventArgs e)
-		{
-			count++;
-			CounterLabel.Text = $"Current count: {count}";
+        private static string MSGraphURL = "https://graph.microsoft.com/v1.0/";
+        private static AuthenticationResult authResult;
 
-			SemanticScreenReader.Announce(CounterLabel.Text);
-		}
-	}
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnSignInClicked(object sender, EventArgs e)
+        {
+            try
+            {
+                // Sign-in user using MSAL and obtain an access token for MS Graph
+                GraphServiceClient graphClient = await SignInAndInitializeGraphServiceClient(scopes);
+
+                // Call the /me endpoint of Graph
+                User graphUser = await graphClient.Me.Request().GetAsync();
+
+                TokenLabel.Text = "Display Name: " + graphUser.DisplayName + "\nBusiness Phone: " + graphUser.BusinessPhones.FirstOrDefault()
+                                    + "\nGiven Name: " + graphUser.GivenName + "\nid: " + graphUser.Id
+                                    + "\nUser Principal Name: " + graphUser.UserPrincipalName;
+            }
+            catch (MsalException msalEx)
+            {
+                TokenLabel.Text = $"Error Acquiring Token:{System.Environment.NewLine}{msalEx}";
+            }
+
+            SignInButton.IsVisible = false;
+            SignOutButton.IsVisible = true;
+
+            SemanticScreenReader.Announce(TokenLabel.Text);
+        }
+
+        private async void OnSignOutClicked(object sender, EventArgs e)
+        {
+            IEnumerable<IAccount> accounts = await PublicClientApp.GetAccountsAsync();
+            IAccount firstAccount = accounts.FirstOrDefault();
+
+            try
+            {
+                await PublicClientApp.RemoveAsync(firstAccount);
+
+                TokenLabel.Text = "User has signed-out";
+            }
+            catch (MsalException ex)
+            {
+                TokenLabel.Text = $"Error signing-out user: {ex.Message}";
+            }
+
+            SignInButton.IsVisible = true;
+            SignOutButton.IsVisible = false;
+        }
+
+        /// <summary>
+        /// Signs in the user and obtains an Access token for MS Graph
+        /// </summary>
+        /// <param name="scopes"></param>
+        /// <returns> Access Token</returns>
+        private static async Task<string> SignInUserAndGetTokenUsingMSAL(string[] scopes)
+        {
+            // Initialize the MSAL library by building a public client application
+            PublicClientApp = PublicClientApplicationBuilder.Create(ClientId)
+                .WithAuthority(Authority)
+                .WithRedirectUri($"https://login.microsoftonline.com/common/oauth2/nativeclient")
+                .WithLogging((level, message, containsPii) =>
+                {
+                    Debug.WriteLine($"MSAL: {level} {message} ");
+                }, LogLevel.Warning, enablePiiLogging: false, enableDefaultPlatformLogging: true)
+                .Build();
+
+            IEnumerable<IAccount> accounts = await PublicClientApp.GetAccountsAsync().ConfigureAwait(false);
+            IAccount firstAccount = accounts.FirstOrDefault();
+
+            try
+            {
+                authResult = await PublicClientApp.AcquireTokenSilent(scopes, firstAccount)
+                                                  .ExecuteAsync();
+            }
+            catch (MsalUiRequiredException ex)
+            {
+                // A MsalUiRequiredException happened on AcquireTokenSilentAsync. This indicates you need to call AcquireTokenAsync to acquire a token
+                Debug.WriteLine($"MsalUiRequiredException: {ex.Message}");
+
+                authResult = await PublicClientApp.AcquireTokenInteractive(scopes)
+                                                  .ExecuteAsync()
+                                                  .ConfigureAwait(false);
+
+            }
+            return authResult.AccessToken;
+        }
+
+        /// <summary>
+        /// Sign in user using MSAL and obtain a token for MS Graph
+        /// </summary>
+        /// <returns>GraphServiceClient</returns>
+        private async static Task<GraphServiceClient> SignInAndInitializeGraphServiceClient(string[] scopes)
+        {
+            GraphServiceClient graphClient = new GraphServiceClient(MSGraphURL,
+                new DelegateAuthenticationProvider(async (requestMessage) =>
+                {
+                    requestMessage.Headers.Authorization = new AuthenticationHeaderValue("bearer", await SignInUserAndGetTokenUsingMSAL(scopes));
+                }));
+
+            return await Task.FromResult(graphClient);
+        }
+    }
 }
