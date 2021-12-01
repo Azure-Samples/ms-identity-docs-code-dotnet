dca1a4b5554182d9d27a44f27032758497bd6be7
Fernando Antivero <v-feanti@microsoft.com>
2021-12-01 16:00:14 -0300

    clean up

--- src/sign-in-xplat/App.xaml.cs
+++ src/sign-in-xplat/App.xaml.cs
@@ -1,11 +1,8 @@
-﻿using Microsoft.Maui;
-using Microsoft.Maui.Controls;
-using Microsoft.Maui.Controls.PlatformConfiguration.WindowsSpecific;
-using Application = Microsoft.Maui.Controls.Application;
+﻿using Application = Microsoft.Maui.Controls.Application;
 
 namespace XPlat
 {
-	public partial class App : Application
+    public partial class App : Application
 	{
 		public App()
 		{
