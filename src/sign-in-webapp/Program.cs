4e8d876572807b46e670b8eae8587813a1eaceac
Fernando Antivero <v-feanti@microsoft.com>
2021-12-15 15:18:10 -0300

    patch (DOCS-CODE-016): [authN] [sign-in] ensure Azure AD v2.0 usage in the canonical webapp (#6)
    * remove legacy code and ensure the usage of Azure AD v2.0
    
    * capitalize proj file name

--- src/sign-in-webapp/Program.cs
+++ src/sign-in-webapp/Program.cs
@@ -1,5 +1,6 @@
 // <ms_docref_import_types>
-using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Identity.Web;
 using Microsoft.Identity.Web.UI;
 // </ms_docref_import_types>
@@ -8,23 +9,20 @@ using Microsoft.Identity.Web.UI;
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 IEnumerable<string>? initialScopes = builder.Configuration["DownstreamApi:Scopes"]?.Split(' ');
 
-builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
-    .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAd"))
-        .EnableTokenAcquisitionToCallDownstreamApi(initialScopes)
-            .AddDownstreamWebApi("DownstreamApi", builder.Configuration.GetSection("DownstreamApi"))
-            .AddInMemoryTokenCaches();
+builder.Services.AddMicrosoftIdentityWebAppAuthentication(builder.Configuration, "AzureAd")
+    .EnableTokenAcquisitionToCallDownstreamApi(initialScopes)
+        .AddDownstreamWebApi("DownstreamApi", builder.Configuration.GetSection("DownstreamApi"))
+        .AddInMemoryTokenCaches();
 // </ms_docref_add_msal>
 
-// <ms_docref_add_default_authz_policies>
-builder.Services.AddAuthorization(options =>
-{
-    options.FallbackPolicy = options.DefaultPolicy;
-});
-// </ms_docref_add_default_authz_policies>
-
 // <ms_docref_add_default_controller_for_sign-in-out>
-builder.Services.AddRazorPages()
-    .AddMicrosoftIdentityUI();
+builder.Services.AddRazorPages().AddMvcOptions(options =>
+    {
+        var policy = new AuthorizationPolicyBuilder()
+                      .RequireAuthenticatedUser()
+                      .Build();
+        options.Filters.Add(new AuthorizeFilter(policy));
+    }).AddMicrosoftIdentityUI();
 // </ms_docref_add_default_controller_for_sign-in-out>
 
 // <ms_docref_enable_authz_capabilities>
