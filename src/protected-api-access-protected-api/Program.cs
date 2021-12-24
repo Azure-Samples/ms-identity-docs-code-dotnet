58fab2f19bb4b9d3ea98e5f38b1cd581d564a0fa
Fernando Antivero <v-feanti@microsoft.com>
2021-12-24 01:48:54 +0000

    add canonical sample for protected api

---
+++ src/protected-api-access-protected-api/Program.cs
@@ -0,0 +1,27 @@
+// <ms_docref_import_types>
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Identity.Web;
+// </ms_docref_import_types>
+
+// <ms_docref_add_msal>
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
+builder.Services.AddAuthorization();
+// </ms_docref_add_msal>
+
+// <ms_docref_enable_authz_capabilities>
+WebApplication app = builder.Build();
+app.UseAuthentication();
+app.UseAuthorization();
+// </ms_docref_enable_authz_capabilities>
+
+// <ms_docref_protect_endpoint>
+app.MapGet("/me", [Authorize()] () =>
+{
+    return "OK";
+});
+// </ms_docref_protect_endpoint>
+
+app.Run();
