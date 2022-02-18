72af591cfa20eb8e7e18eca9f4ec65ac6e3f5458
Fernando Antivero <v-feanti@microsoft.com>
2022-02-18 17:24:22 +0000

    Address PR Feedback: use http client to make the Graph Api call

--- src/sign-in-blazorwasm/Program.cs
+++ src/sign-in-blazorwasm/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorWasm;
 
@@ -11,6 +12,18 @@ builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.
 builder.Services.AddMsalAuthentication(options =>
 {
     builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
+});
+
+builder.Services.AddScoped(sp =>
+{
+    var authorizationMessageHandler =
+        sp.GetRequiredService<AuthorizationMessageHandler>();
+    authorizationMessageHandler.InnerHandler = new HttpClientHandler();
+    authorizationMessageHandler.ConfigureHandler(
+        authorizedUrls: new[] { "https://graph.microsoft.com/v1.0" },
+        scopes: new[] { "User.Read" });
+
+    return new HttpClient(authorizationMessageHandler);
 });
 
 await builder.Build().RunAsync();
