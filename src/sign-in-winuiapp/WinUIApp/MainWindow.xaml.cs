c18e52afe4eee8ef68a7135bd220ffa8ab0c3834
Fernando Antivero <v-feanti@microsoft.com>
2022-03-18 17:05:59 -0300

    add usage for silent acq

--- src/sign-in-winuiapp/WinUIApp/MainWindow.xaml.cs
+++ src/sign-in-winuiapp/WinUIApp/MainWindow.xaml.cs
@@ -23,8 +23,6 @@ namespace WinUIApp
         // MSAL client to the lifecycle of this form.
         private readonly IPublicClientApplication msalPublicClientApp;
 
-        private bool _isAuthenticated = false;
-
         public MainWindow()
         {
             this.InitializeComponent();
@@ -44,70 +42,69 @@ namespace WinUIApp
                 .Build();
         }
 
-        private async void Sign_Click(object sender, RoutedEventArgs e)
+        private async void SignIn_Click(object sender, RoutedEventArgs e)
         {
-            if (!_isAuthenticated)
+            AuthenticationResult? msalAuthenticationResult = null;
+
+            // Acquire a cached access token for Microsoft Graph if one is available from a prior
+            // execution of this process.
+            var accounts = await msalPublicClientApp.GetAccountsAsync();
+            if (accounts.Any())
             {
-                AuthenticationResult? msalAuthenticationResult = null;
-
-                // Acquire a cached access token for Microsoft Graph if one is available from a prior
-                // execution of this process.
-                var accounts = await msalPublicClientApp.GetAccountsAsync();
-                if (accounts.Any())
+                try
                 {
-                    try
-                    {
-                        // Will return a cached access token if available, refreshing if necessary.
-                        msalAuthenticationResult = await msalPublicClientApp.AcquireTokenSilent(
-                            new[] { "https://graph.microsoft.com/User.Read" },
-                            accounts.First())
-                            .ExecuteAsync();
-                    }
-                    catch (MsalUiRequiredException)
-                    {
-                        // Nothing in cache for this account + scope, and interactive experience required.
-                    }
-                }
-
-                if (msalAuthenticationResult == null)
-                {
-                    // This is likely the first authentication request in the application, so calling
-                    // this will launch the user's default browser and send them through a login flow.
-                    // After the flow is complete, the rest of this method will continue to execute.
-                    msalAuthenticationResult = await msalPublicClientApp.AcquireTokenInteractive(
-                        new[] { "https://graph.microsoft.com/User.Read" })
+                    // Will return a cached access token if available, refreshing if necessary.
+                    msalAuthenticationResult = await msalPublicClientApp.AcquireTokenSilent(
+                        new[] { "https://graph.microsoft.com/User.Read" },
+                        accounts.First())
                         .ExecuteAsync();
                 }
-
-                // Call Microsoft Graph using the access token acquired above.
-                using var graphRequest = new HttpRequestMessage(HttpMethod.Get, "https://graph.microsoft.com/v1.0/me");
-                graphRequest.Headers.Authorization = new AuthenticationHeaderValue("Bearer", msalAuthenticationResult.AccessToken);
-                var graphResponseMessage = await httpClient.SendAsync(graphRequest);
-                graphResponseMessage.EnsureSuccessStatusCode();
-
-                // Present the results to the user (formatting the json for readability)
-                using var graphResponseJson = JsonDocument.Parse(await graphResponseMessage.Content.ReadAsStreamAsync());
-                graphCallResultTextBlock.Text = JsonSerializer.Serialize(graphResponseJson, new JsonSerializerOptions { WriteIndented = true, Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping });
-                var tokenWasFromCache = TokenSource.Cache == msalAuthenticationResult.AuthenticationResultMetadata.TokenSource;
-                tokenAcquisitionTextBlock.Text = $"{(tokenWasFromCache ? "Cached" : "Newly Acquired")} (Expires: {msalAuthenticationResult.ExpiresOn:R})";
-
-                this._isAuthenticated = true;
-                signInOutButton.Content = "Sign Out";
-            }
-            else
-            {
-                // Signing out is removing all cached tokens, meaning the next token request will
-                // require the user to sign in.
-                foreach (var account in (await msalPublicClientApp.GetAccountsAsync()).ToList())
+                catch (MsalUiRequiredException)
                 {
-                    await msalPublicClientApp.RemoveAsync(account);
+                    // Nothing in cache for this account + scope, and interactive experience required.
                 }
-
-                // Show the call to action and hide the results.
-                graphCallResultTextBlock.Text = string.Empty;
-                this._isAuthenticated = false;
-                signInOutButton.Content = "Sign In";
             }
+
+            if (msalAuthenticationResult == null)
+            {
+                // This is likely the first authentication request in the application, so calling
+                // this will launch the user's default browser and send them through a login flow.
+                // After the flow is complete, the rest of this method will continue to execute.
+                msalAuthenticationResult = await msalPublicClientApp.AcquireTokenInteractive(
+                    new[] { "https://graph.microsoft.com/User.Read" })
+                    .ExecuteAsync();
+            }
+
+            // Call Microsoft Graph using the access token acquired above.
+            using var graphRequest = new HttpRequestMessage(HttpMethod.Get, "https://graph.microsoft.com/v1.0/me");
+            graphRequest.Headers.Authorization = new AuthenticationHeaderValue("Bearer", msalAuthenticationResult.AccessToken);
+            var graphResponseMessage = await httpClient.SendAsync(graphRequest);
+            graphResponseMessage.EnsureSuccessStatusCode();
+
+            // Present the results to the user (formatting the json for readability)
+            using var graphResponseJson = JsonDocument.Parse(await graphResponseMessage.Content.ReadAsStreamAsync());
+            graphCallResultTextBlock.Text = JsonSerializer.Serialize(graphResponseJson, new JsonSerializerOptions { WriteIndented = true, Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping });
+            var tokenWasFromCache = TokenSource.Cache == msalAuthenticationResult.AuthenticationResultMetadata.TokenSource;
+            tokenAcquisitionTextBlock.Text = $"Access Token: {(tokenWasFromCache ? "Cached" : "Newly Acquired")} (Expires: {msalAuthenticationResult.ExpiresOn:R})";
+
+            this.signInCallToActionTextBlock.Visibility = Visibility.Collapsed;
+            this.authorizedPanel.Visibility = Visibility.Visible;
+        }
+
+        private async void SignOut_Click(object sender, RoutedEventArgs e)
+        {
+            // Signing out is removing all cached tokens, meaning the next token request will
+            // require the user to sign in.
+            foreach (var account in (await msalPublicClientApp.GetAccountsAsync()).ToList())
+            {
+                await msalPublicClientApp.RemoveAsync(account);
+            }
+
+            // Show the call to action and hide the results.
+            graphCallResultTextBlock.Text = string.Empty;
+            tokenAcquisitionTextBlock.Text = "";
+            this.signInCallToActionTextBlock.Visibility = Visibility.Visible;
+            this.authorizedPanel.Visibility = Visibility.Collapsed;
         }
     }
 }
