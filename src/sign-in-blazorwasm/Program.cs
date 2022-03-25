a8a48d715e6d662a1ce765a21c189033e04b3736
Fernando Antivero <v-feanti@microsoft.com>
2022-03-25 20:10:41 +0000

    remove unused service

--- src/sign-in-blazorwasm/Program.cs
+++ src/sign-in-blazorwasm/Program.cs
@@ -7,8 +7,6 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
 builder.Services.AddMsalAuthentication(options =>
 {
     builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
