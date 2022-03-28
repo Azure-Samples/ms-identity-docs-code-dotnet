c05264d1584dbe3b507cc97c1a9aa694de017c28
Fernando Antivero <v-feanti@microsoft.com>
2022-03-28 19:03:09 -0300

    feat (DOCS-CODE-021): [show pattern usage] add some text for refreshing for cached token and other minor fixes (#31)
    * remove unused service
    
    * add text for our users to refersh for cached token, aligned UI with other tutorials, removed graph sdk reference
    
    * bug fix: use cached token
    
    * Address PR Feedback: reduce ambuiguity around the scope
    
    * format json output
    
    * add screenshot with new UX

--- src/sign-in-blazorwasm/Program.cs
+++ src/sign-in-blazorwasm/Program.cs
@@ -7,11 +7,11 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
 builder.Services.AddMsalAuthentication(options =>
 {
     builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
+    options.ProviderOptions.DefaultAccessTokenScopes
+            .Add("https://graph.microsoft.com/User.Read");
 });
 
 builder.Services.AddScoped(sp =>
