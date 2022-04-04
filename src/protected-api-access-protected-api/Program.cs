2b82235085d0e6db2636708e6699cfa8adf1daee
Fernando Antivero <v-feanti@microsoft.com>
2022-04-04 14:21:34 +0000

    show usage pattern, align formats among tutos, standarize urls, add comments in README

--- src/protected-api-access-protected-api/Program.cs
+++ src/protected-api-access-protected-api/Program.cs
@@ -1,3 +1,4 @@
+using System.Text.Json;
 // <ms_docref_import_types>
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
@@ -9,6 +10,9 @@ WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"))
                 .EnableTokenAcquisitionToCallDownstreamApi()
+// The access token came from the in-memory token cache, which maintains
+// the on-behalf-of access token, per user-assertion, based on the provided access
+// token to this API.
                 .AddInMemoryTokenCaches()
                 .AddDownstreamWebApi("GraphApi", builder.Configuration.GetSection("GraphApi"));
 builder.Services.AddAuthorization();
@@ -21,7 +25,12 @@ app.UseAuthorization();
 // </ms_docref_enable_authz_capabilities>
 
 // <ms_docref_protect_endpoint>
-app.MapGet("/me", [Authorize()] async (IDownstreamWebApi downstreamWebApi) => await downstreamWebApi.CallWebApiForUserAsync("GraphApi"));
+app.MapGet("/api/me", [Authorize()] async (IDownstreamWebApi downstreamWebApi) => {
+  var response = await downstreamWebApi.CallWebApiForUserAsync("GraphApi").ConfigureAwait(false);
+
+  var graphApiResponse = await response.Content.ReadFromJsonAsync<JsonDocument>().ConfigureAwait(false);
+  return JsonSerializer.Serialize(graphApiResponse, new JsonSerializerOptions { WriteIndented = true });
+});
 // </ms_docref_protect_endpoint>
 
 app.Run();
