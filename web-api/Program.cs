b9444852fc3252583b509a87a2902bd39bd091ec
Marsh Macy <mmacy@users.noreply.github.com>
2022-10-13 21:46:43 -0700

    test API access w/curl

--- web-api/Program.cs
+++ web-api/Program.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.Identity.Web;
+using Microsoft.IdentityModel.Logging;
+using System.Security.Claims;
 // </ms_docref_import_types>
 
 // <ms_docref_add_msal>
@@ -11,9 +13,15 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 builder.Services.AddAuthorization(config =>
 {
     config.AddPolicy("AuthZPolicy", policyBuilder =>
-        policyBuilder.Requirements.Add(new ScopeAuthorizationRequirement() { RequiredScopesConfigurationKey = $"AzureAd:Scopes" }));
+        policyBuilder.Requirements.Add(
+            new ScopeOrAppPermissionAuthorizationRequirement() {
+                RequiredScopesConfigurationKey = $"AzureAd:Scopes",
+                RequiredAppPermissionsConfigurationKey = $"AzureAd:AppPermissions" }
+            ));
 });
 // </ms_docref_add_msal>
+
+IdentityModelEventSource.ShowPII = true;
 
 // <ms_docref_enable_authz_capabilities>
 WebApplication app = builder.Build();
@@ -25,10 +33,19 @@ var weatherSummaries = new[]
 {
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
+weatherSummaries.Append("BLARG");
 
 // <ms_docref_protect_endpoint>
-app.MapGet("/weatherforecast", [Authorize(Policy = "AuthZPolicy")] () =>
+app.MapGet("/weatherforecast", [Authorize(Policy = "AuthZPolicy")] (HttpContext context) =>
 {
+    ClaimsPrincipal apiUser = context.User;
+    string? userObjectID = apiUser.FindFirst(ClaimConstants.ObjectId)?.Value;
+    
+    if (!String.IsNullOrEmpty(userObjectID) && userObjectID == "3d6908ca-a97c-453b-ab49-3d1a407b6af6")
+    {
+        Console.WriteLine("==-==-==-==-==-==-==-==-== CASEY CHECKED THE WEATHER ==-==-==-==-==-==-==-==-==");
+    }
+    
     var forecast = Enumerable.Range(1, 5).Select(index =>
        new WeatherForecast
        (
@@ -41,6 +58,15 @@ app.MapGet("/weatherforecast", [Authorize(Policy = "AuthZPolicy")] () =>
 })
 .WithName("GetWeatherForecast");
 // </ms_docref_protect_endpoint>
+
+// TESTING ONLY
+// Call with:
+// curl -X POST https://localhost:5001/addweathersummary?summary=BLARG -ki -H "Content-Type: application/x-www-form-urlencoded"
+app.MapPost("/addweathersummary", (string summary) =>
+{
+    return weatherSummaries.Append(summary);
+})
+.WithName("AddWeatherSummary");
 
 app.Run();
 
